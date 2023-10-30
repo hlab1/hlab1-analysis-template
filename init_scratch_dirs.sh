b95a6aca902dc697f9acee3cb35d606acfa8bfe9
Shao-shan Carol Huang <shhuang1@gmail.com>
2023-10-30 10:41:35 -0400

    Added making symblinks to envs

--- init_scratch_dirs.sh
+++ init_scratch_dirs.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-echo "Making data,raw,results directories in $1 and symlinks to them in current diretory"
+echo "Making data,envs,raw,results directories in $1 and symlinks to them in current diretory"
 mkdir -p $1
-for d in data raw results
+for d in data envs raw results 
 do
 
     mkdir $1/$d
     ln -s $1/$d $d
 done
+
+mkdir $1/envs/singularity_images
+mkdir $1/envs/singularity_overlay
 
 echo "Done!"
